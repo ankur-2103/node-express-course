f3782c36f9dcee73ef864f39af022cd8d807ff33
ankur-2103 <85174725+ankur-2103@users.noreply.github.com>
2021-08-17 12:38:13 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyPraser = require('body-parser');
 const app = express();
+app.use(bodyPraser.json());
 
 const mockUserData=[
     {name:'Ankur'},
@@ -21,6 +23,27 @@ app.get('/users/:id', function(req, res){
         message: 'successfully got users. Nice!',
         users: req.params.id
     })
+})
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockname = "Ankur";
+    const mockpassword = "Aarya";
+
+    if(username==mockname && password==mockpassword){
+        res.json({
+            success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+        })
+    }else{
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+       })
+    }
 })
 
 app.listen(8000,function(){
