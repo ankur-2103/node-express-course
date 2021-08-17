8bad2786ae6acad94eecf5342ea89d286c6ee904
ankur-2103 <85174725+ankur-2103@users.noreply.github.com>
2021-08-17 12:19:22 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+    {name:'Ankur'},
+    {name:'Aarya'}
+]
+
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
 
 app.listen(8000,function(){
     console.log("server is running")
