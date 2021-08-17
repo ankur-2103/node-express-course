f049ede1961374e5f9171760b11859da3a69ac52
ankur-2103 <85174725+ankur-2103@users.noreply.github.com>
2021-08-17 12:26:24 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get('/users', function(req, res){
     })
 })
 
+app.get('/users/:id', function(req, res){
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: req.params.id
+    })
+})
+
 app.listen(8000,function(){
     console.log("server is running")
 })
